a187dd5c638d629c0df6e64f4103bb87dabe86d6
Usuario <Usuario@10.22.148.98>
2018-10-04 11:12:47 +0100

    Kata2 ver 1

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Kata2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] data;
+        data = new int[] {1, -1, 0, 3, 1, 1, 1, 0, 4, 5, 7, 2, -1, 4, 3, 2, 0, 1};
+        Map<Integer, Integer> histogram; 
+        histogram = new HashMap<>();
+        for (int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])) {
+                histogram.put(data[i], histogram.get(data[i])+1);
+            } else {
+                histogram.put(data[i], 1);
+            }
+        }
+        histogram.entrySet().forEach((entry) -> {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }); 
+    }
+}
